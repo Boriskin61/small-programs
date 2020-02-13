2fd6b39fda1a7455854a16cbb2e2bb3a484e2991
Boriskin61 <borisovpetr@mail.ru>
2020-02-14 00:56:12 +0300

    split vowels

--- consonants.c
+++ consonants.c
@@ -3,21 +3,23 @@
 
 int main(int argc, char* argv[]) {
 	char *line;
-	int i, v, len;
+	int i, c, len;
 	if (argc < 2) {
 		return 1;
 	} else {
 		line = argv[1];
-		v = 0, len = strlen(line);
+		c = 0, len = strlen(line);
 	}
 
 	for (i = 0; i < len; ++i) {
         if (line[i]=='a' || line[i]=='e' || line[i]=='i' || line[i]=='o' || line[i]=='u' || line[i]=='y'
 		|| line[i]=='A' || line[i]=='E' || line[i]=='I' || line[i]=='O' || line[i]=='U' || line[i]=='Y') {
-            ++v;
-		}
+            continue;
+		} else if (line[i]>='a' && line[i]<='z' || line[i]>='A' && line[i]<='Z') {
+            ++c;
+        }
 	}
 
-	printf("Vowels: %d\n", v);
+	printf("Consonants: %d\n", c);
 	return 0;
 }
---
+++ printable.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+	char *line;
+	int i, print, digs, chars, other, len;
+	if (argc < 2) {
+		return 1;
+	} else {
+		line = argv[1];
+		print = 0, digs = 0, chars = 0, other = 0, len = strlen(line);
+	}
+
+	for (i = 0; i < len; ++i) {
+        if (line[i]>='a' && line[i]<='z' || line[i]>='A' && line[i]<='Z') {
+            ++chars; ++print;
+        } else if (line[i] >= '0' && line[i] <= '9') {
+            ++digs; ++print;
+        } else if (line[i] >= ' ' && line[i] <= '\x7f') {
+            ++other; ++print;
+        }
+	}
+
+	printf(
+        "Printable: %d\n"
+        "Characters: %d\n"
+        "Digits: %d\n"
+        "Other printable: %d\n",
+        print, chars, digs, other
+    );
+	return 0;
+}
